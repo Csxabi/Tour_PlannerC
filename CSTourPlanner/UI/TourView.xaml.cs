f64710e2dcd9d83c6854b8ad0e7768f6eef0c9e9
mahomaddah <72700630+Csxabi@users.noreply.github.com>
2021-09-15 18:32:33 +0300

    Basic UI

--- CSTourPlanner/UI/TourView.xaml.cs
+++ CSTourPlanner/UI/TourView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CSTourPlanner
+namespace CSTourPlanner.UI
 {
     /// <summary>
-    /// MainWindow.xaml etkileşim mantığı
+    /// TourView.xaml etkileşim mantığı
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class TourView : Window
     {
-        public MainWindow()
+        public TourView()
         {
             InitializeComponent();
         }
